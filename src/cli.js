58084b8b73b3560407d770812e6b2075ad40b094
sina-byn <sina.sina.bayan@protonmail.com>
2023-07-28 21:08:05 +0330

    parseArgsIntoOptions func added

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,43 @@
+const arg = require('arg');
 const chalk = require('chalk');
 
+const options = ['-c', '--config'];
+
 const showError = (message, option) => {
-    console.error(chalk.bold.red(message), chalk.bold.blue(option));
-    return {};
+  console.error(chalk.bold.red(message), chalk.bold.blue(option));
+  return {};
+};
+
+const parseArgsIntoOptions = rawArgs => {
+  rawArgs = rawArgs.slice(2);
+  const lastArg = rawArgs[rawArgs.length - 1];
+
+  if (options.includes(lastArg)) {
+    return showError('option requires argument:', '-c | --config');
+  }
+
+  const args = arg(
+    {
+      '--config': String,
+      '-c': '--config',
+    },
+    { argv: rawArgs, permissive: true }
+  );
+
+  if (!('--config' in args)) {
+    return showError('missing required argument:', '-c | --config');
+  }
+
+  return {
+    configPath: args['--config'] || 'eraser.config.json',
+  };
 };
 
 const cli = args => {
-    console.log(args);
+  const { configPath = null } = parseArgsIntoOptions(args);
+
+  if (!configPath) return;
+  console.log(configPath);
 };
 
 module.exports = cli;
