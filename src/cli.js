e717d9ad7fc3439f89782c88a118a9714cf27461
sina-byn <sina.sina.bayan@protonmail.com>
2023-07-28 21:02:24 +0330

    showError func added

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,10 @@
+const chalk = require('chalk');
+
+const showError = (message, option) => {
+    console.error(chalk.bold.red(message), chalk.bold.blue(option));
+    return {};
+};
+
 const cli = args => {
     console.log(args);
 };
